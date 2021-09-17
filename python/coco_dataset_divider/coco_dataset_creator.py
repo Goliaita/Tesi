4a792642aa2f7f16379e8e56fe1f39a15c6b46c8
Goliaita <davide.basile01@gmail.com>
2021-09-17 19:45:47 +0200

    alcune modifiche è richiesta revisione al coco dataset creator

--- python/coco_dataset_divider/coco_dataset_creator.py
+++ python/coco_dataset_divider/coco_dataset_creator.py
@@ -1,26 +1,42 @@
 import json
 import argparse
 import os
-import os.path as osp
+import traceback
 import random
-import math
-
+import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 
+
+
 parser = argparse.ArgumentParser(
     description='Transform labeled images into coco format')
-parser.add_argument("--path_label", type=str, required=True, help="Path to labeled images")
+parser.add_argument("--path_label", type=str, required=False, help="Path to labeled images", default="./images/")
 parser.add_argument("--path_images", type=str, required=True, help="Path to images")
 parser.add_argument("--out_path", type=str, required=True, help="Path where save the file")
+parser.add_argument("--saved_data", type=str, required=False, help="Path where are saved the segmentation about plants",
+    default=None)
+parser.add_argument("--mpi", type=bool, required=False,
+    help="Run the program as a parallel version (must run with mpiexec) (only for the reading part)",
+    default=False)
 
 args = parser.parse_args()
 
+if args.mpi:
+    try:
+        from mpi4py import MPI
+        mpi = True
+    except Exception:
+        traceback.print_exc()
+        print("Error no mpi package found running sequential mode")
+        mpi = False
+else:
+    mpi = False
 
-def order_segmentation(segmented_array):
+def order_segmentation(segmented_array, index):
     array_in = segmented_array.copy()
     array_in = array_in.reshape((-1,2))
     array_buff = []
@@ -29,10 +45,21 @@ def order_segmentation(segmented_array):
 
     for i in range(2, len(segmented_array), 2):
         counter = 100
-        next = []
 
         for k in range(array_in.shape[0]-1):
-            buff = np.linalg.norm([array_buff[i-2]-array_in[k,0],array_buff[i-1]-array_in[k,1]])
+            try:
+                buff = np.linalg.norm([array_buff[i-2]-array_in[k,0],array_buff[i-1]-array_in[k,1]])
+            except Exception:
+                traceback.print_exc()
+                if mpi:
+                    print("error from rank: {} at {} and {} with array_in: {}, array_buff: {}".format(rank, k, i, array_in.shape, array_buff.shape))    
+                else:
+                    print("error at index: {}, with element {} and {} with array_in: {}, array_buff: {}"
+                        .format(index, k, i, array_in.shape, array_buff.shape))
+                    print(next)
+                sys.stdout.flush()
+                exit()
+
             if counter > buff:
                 counter = buff
                 next = [array_in[k,0], array_in[k,1]]
@@ -46,12 +73,13 @@ def order_segmentation(segmented_array):
     return array_buff
 
 
-def coco_label_creator(label_path: str):
+def coco_label_creator(label_path: str, ids=None, rank=0):
     listed_images = os.listdir(label_path)
     
     json_image = []
     image_colors = []
-
+    if ids is not None:
+        listed_images = listed_images[ids[0]:ids[1]]
 
     l = 0
     for img_name in listed_images:
@@ -87,8 +115,9 @@ def coco_label_creator(label_path: str):
                     exit()
 
         image_colors = np.append(image_colors, len(colors))
-        if (l % 100) == 0:
-            print("Examinated {} files".format(l))
+        if (l % 10) == 0:
+            print("Rank: {}, Examinated {} files".format(rank, l))
+            sys.stdout.flush()
         l += 1
 
     return json_image, image_colors
@@ -124,6 +153,7 @@ def compose_json(labels, colors, img_path):
     })
 
     print("Added the categories")
+    sys.stdout.flush()
 
     listed_imgs = os.listdir(img_path)
     id = 0
@@ -156,6 +186,7 @@ def compose_json(labels, colors, img_path):
     id = 0
 
     print("Added all images")
+    sys.stdout.flush()
 
     for i in range(max_id):
         for j in range(int(colors[i])):
@@ -179,7 +210,7 @@ def compose_json(labels, colors, img_path):
             random_number = random.randint(0,16777215)
             hex_number = str(hex(random_number))
             hex_number ='#'+ hex_number[2:]
-            segmentation = order_segmentation(segmentation)
+            segmentation = order_segmentation(segmentation, i)
 
             buff_json = {
                 "id": id,
@@ -199,19 +230,113 @@ def compose_json(labels, colors, img_path):
     return composed_json
 
 if __name__=="__main__":
+    if args.saved_data is None:
 
-    label, colors = coco_label_creator(args.path_label)
+        if mpi:
+            world = MPI.COMM_WORLD
+            agents_number = world.Get_size()
+            rank = world.Get_rank()
+            listed = os.listdir(args.path_label)
+            portion = len(listed) / agents_number
+            ids = [int(rank * portion), int((rank * portion) + portion)]
+            print("Agent ", rank, " got ", portion, " rows of dataset")
+            sys.stdout.flush()
+        else:
+            ids = None
+            rank = 0
+        
+        
+        label, colors = coco_label_creator(args.path_label, ids, rank)
 
-    print("Finished to extract shape from images")
-    
-    label = label.reshape((-1, 4))
-    
-    my_json = compose_json(label, colors, args.path_images)
+        if mpi:
+            print("Rank: {} finished to extract shape from images".format(rank))
+        else:
+            print("Finished to extract shape from images")
 
-    print("Json composed writing out...")
+        sys.stdout.flush()
 
-    coco_file = open(args.out_path + "/komatsuna_coco.json", "w+")
+        label = label.reshape((-1, 4))
 
-    coco_file.write(json.dumps(my_json))
+        if mpi:
+            world.Barrier()
+            results = world.gather(label, root=0) 
+            results_colors = world.gather(colors, root=0)
+            
+            if rank == 0:
+                sys.stdout.flush()
+                merge_data = []
+                merge_colors = []
+                for j in results:
+                    merge_data = np.append(merge_data, j)
+                for j in results_colors:
+                    merge_colors = np.append(merge_colors, j)
+                merge_data = merge_data.reshape((-1,4))
+                merge_colors = merge_colors.reshape((-1,1))
 
-    print("... Wrote finished the task")
+
+                saving_data = open("./data_saved.txt", "w+")
+                saving_colors = open("./colors_saved.txt", "w+")
+
+                saving_data.write(str(merge_data.tolist()))
+                saving_colors.write(str(merge_colors.tolist()))
+        else:
+            saving_data = open("./data_saved.txt", "w+")
+            saving_colors = open("./colors_saved.txt", "w+")
+
+            saving_data.write(str(label))
+            saving_colors.write(str(colors))
+
+    else: 
+        saved_data = open(args.saved_data + "data_saved.txt", "r")
+        saved_colors = open(args.saved_data + "colors_saved.txt", "r")
+
+        
+        label_buff = saved_data.read()
+        colors_buff = saved_colors.read()
+
+        label_buff = label_buff.replace("[", '')
+        label_buff = label_buff.replace("]", '')
+        label_buff = label_buff.replace(",", '')
+        label_buff = label_buff.replace(".0", '')
+
+        colors_buff = colors_buff.replace("[", '')
+        colors_buff = colors_buff.replace("]", '')
+        colors_buff = colors_buff.replace(",", '')
+        colors_buff = colors_buff.replace(".0", '')
+
+        label  = np.asarray(label_buff.split(" "), dtype=int)
+        colors = np.asarray(colors_buff.split(" "), dtype=int)
+
+
+        label = label.reshape(-1, 4)
+        print("data loaded")
+
+    if mpi:
+        if rank == 0:
+            my_json = compose_json(label, colors, args.path_images)
+
+            print("Json composed writing out...")
+
+            sys.stdout.flush()
+
+            coco_file = open(args.out_path + "/komatsuna_coco.json", "w+")
+
+            coco_file.write(json.dumps(my_json))
+
+            print("... Wrote finished the task")
+            sys.stdout.flush()
+    else:
+        my_json = compose_json(label, colors, args.path_images)
+
+        print("Json composed writing out...")
+
+        sys.stdout.flush()
+
+        coco_file = open(args.out_path + "/komatsuna_coco.json", "w+")
+
+        coco_file.write(json.dumps(my_json))
+
+        print("... Wrote finished the task")
+        sys.stdout.flush()
+
+
